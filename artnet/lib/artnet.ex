c926bb72ca42e2ef566207ae4be89109b3678a89
evert <evert@servert.be>
2020-12-22 20:50:24 +0100

    poncho structure

---
+++ artnet/lib/artnet.ex
@@ -0,0 +1,18 @@
+defmodule Artnet do
+  @moduledoc """
+  Documentation for `Artnet`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Artnet.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ artnet/test/artnet_test.exs
@@ -0,0 +1,8 @@
+defmodule ArtnetTest do
+  use ExUnit.Case
+  doctest Artnet
+
+  test "greets the world" do
+    assert Artnet.hello() == :world
+  end
+end
