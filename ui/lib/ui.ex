466d06266ea6580db06d9f087688ec0ce85e3fe6
evert <evert@servert.be>
2021-06-27 21:16:13 +0200

    Create dmx bitstring from lights map

--- ui/lib/ui.ex
+++ ui/lib/ui.ex
@@ -31,10 +31,9 @@ defmodule Ui do
     IO.puts("Init")
     # read the config
     Phoenix.PubSub.subscribe(@server, "events")
-    lights = %{:living => %Light{ui_name: "licht bureau", ui_group: "leefruimte", ui_order: 1, r: 0, g: 0, b: 0, w: 190, default_w: 120, dmx_channel: 0},
-                :kitchen => %Light{ui_name: "keukeneiland", ui_group: "leefruimte", ui_order: 2, r: 0, g: 0, b: 0, w: 220, default_w: 200, dmx_channel: 1},
-                :bathroom => %Light{ui_name: "bathroom light", ui_group: "badkamer", ui_order: 3, r: 0, g: 0, b: 0, w: 90, default_w: 240, dmx_channel: 2}
-  }
+    lights = %{:living => %Light{ui_name: "licht bureau", ui_group: "leefruimte", ui_order: 1, r: 0, g: 0, b: 0, w: 190, default_w: 120, dmx_channel_w: 0, dmx_channel_r: 1, dmx_channel_g: 2, dmx_channel_b: 3, rgb: true},
+                :kitchen => %Light{ui_name: "keukeneiland", ui_group: "leefruimte", ui_order: 2, r: 0, g: 0, b: 0, w: 220, default_w: 200, dmx_channel_w: 6, rgb: false},
+                :bathroom => %Light{ui_name: "bathroom light", ui_group: "badkamer", ui_order: 3, r: 0, g: 0, b: 0, w: 90, default_w: 240, dmx_channel_w: 9, rgb: false}}
     {:ok, lights}
   end
 
@@ -42,6 +41,9 @@ defmodule Ui do
   def handle_info({:state, lights}, state) do
     IO.inspect(state)
     IO.puts("Received a lights state update")
+    dmx_bitstring = Dmx.create_dmx_bitstring(lights)
+    IO.inspect(dmx_bitstring)
+    
     #state = assign(state, lights: lights)
     {:noreply, lights}
   end
